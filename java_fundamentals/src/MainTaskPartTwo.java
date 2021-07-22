2c29d315658b0d3dd18e0995d61fc99c4ebc3b67
ermekbaeva <ermekbaevameerim@gmail.com>
2021-07-22 15:37:33 +0300

    added sum of numbers task

--- java_fundamentals/src/MainTaskPartTwo.java
+++ java_fundamentals/src/MainTaskPartTwo.java
@@ -6,8 +6,7 @@ public class MainTaskPartTwo {
         String data = scanner.nextLine();
         String[] array = data.split(" ");
         System.out.println(sumNumbers(array));
-        int monthNum = scanner.nextInt();
-        System.out.println(month(monthNum));
+
     }
     static int sumNumbers(String[] array){
         int[] num = new int[array.length];
@@ -18,36 +17,5 @@ public class MainTaskPartTwo {
         }
         return sum;
     }
-    static String month(int num){
-        String monthName;
-        switch (num){
-            case 1: monthName="Jan";
-            break;
-            case 2: monthName="Feb";
-                break;
-            case 3: monthName="Mar";
-                break;
-            case 4: monthName="Apr";
-                break;
-            case 5: monthName="May";
-                break;
-            case 6: monthName="Jun";
-                break;
-            case 7: monthName="Jul";
-                break;
-            case 8: monthName="Aug";
-                break;
-            case 9: monthName="Sep";
-                break;
-            case 10: monthName="Oct";
-                break;
-            case 11: monthName="Nov";
-                break;
-            case 12: monthName="Dec";
-                break;
-            default: monthName = "Incorrect! Enter number of month";
-                break;
-        }
-        return monthName;
-    }
+
 }
