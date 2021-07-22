fc592e0ab8c3ce14297adda3d735a1e7dfe53cba
ermekbaeva <ermekbaevameerim@gmail.com>
2021-07-22 15:36:15 +0300

    added 1 and 2 task of main

--- java_fundamentals/src/MainTask.java
+++ java_fundamentals/src/MainTask.java
@@ -8,23 +8,16 @@ public class MainTask {
         String name = scanner.nextLine();
         sayHi(name);
         textReverse(name);
-        randomNumbers(7);
+    }
 
+    static void sayHi(String name) {
+        System.out.println("Hi, " + name + "!");
     }
-    static void sayHi(String name){
-        System.out.println("Hi, "+ name + "!");
-    }
-    static void textReverse(String text){
+
+    static void textReverse(String text) {
         StringBuilder rev = new StringBuilder(text);
         System.out.println(rev.reverse());
     }
-    static void randomNumbers(int count){
-        Random random = new Random();
-        int[] numbers = new int[count];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i]=random.nextInt(10);
-        }
-        System.out.println(Arrays.toString(numbers));
-    }
+
 
 }
